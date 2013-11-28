85659f01c83b6e774056953e512850d37d66c9dc
Okeke Emmanuel <emmanix2002@gmail.com>
2013-11-28 12:40:15 +0100

    added an optional parameter so that the items to be cached can be specified if needed, else it defaults to the tree

--- directory/cache.py
+++ directory/cache.py
@@ -4,7 +4,7 @@ import shelve
 
 class AwsDirectoryCache:
 	'''This class performs the sole purpose of maintaining a cache for all directories.
-	
+
 	It maintains a cache containing metadata relevant to files on a per folder basis for directories/files
 	uploaded using the script.'''
 	def __init__(self, path=None, tree=None, db_name="AwsDirectoryCache.db", cache_key=None):
@@ -12,20 +12,20 @@ class AwsDirectoryCache:
 		self.setPath(path)
 		self.setTree(tree)
 		self.setCacheKey(cache_key)
-	
+
 	def setCacheKey(self, cache_key):
 		"""Sets the value for the cache key."""
 		if cache_key is None or len(cache_key.strip()) == 0:
 			self.cache_key = self.path
 		else:
 			self.cache_key = cache_key
-	
+
 	def setDbName(self, db_name):
 		"""Sets the name of the database to be used by the cache manager."""
 		if db_name is None or len(db_name.strip()) == 0:
 			raise TypeError("The DB name cannot be None or an empty string")
 		self.db_name = db_name
-	
+
 	def setPath(self, path):
 		"""Sets the path to the directory whose data is to be cached."""
 		self.path = None
@@ -34,7 +34,7 @@ class AwsDirectoryCache:
 				raise IOError("The path {0} does not exist on this machine".format(path))
 			else:
 				self.path = path
-	
+
 	def setTree(self, tree):
 		"""Sets the tree that will be used to generate the cache data to be stored."""
 		self.tree = None
@@ -45,7 +45,7 @@ class AwsDirectoryCache:
 				self.tree = tree
 			else:
 				raise KeyError("One or more of the keys (path,relative_path,last_modified_time) was not found in your entries")			
-	
+
 	def getCache(self):
 		"""Gets the cache data for the specified path in the db if it exists else it throws an error."""
 		shelve_db = shelve.open(self.db_name)
@@ -54,15 +54,20 @@ class AwsDirectoryCache:
 		cache_data = shelve_db[self.cache_key]
 		shelve_db.close()
 		return cache_data
-	
-	def setCache(self):
+
+	def setCache(self, cache_items=None):
 		"""Sets the new data for the value of this path in the cache."""
+		items = None
+		if cache_items is None:
+			items = self.tree
+		else:
+			items = cache_items
 		if self.path is None or self.tree is None:
 			raise EnvironmentError("Either or both the path and path tree data are set to None")
 		cache_data = [{"path": entry['path'], "relative_path": entry['relative_path'],
-					   "last_modified_time": entry['last_modified_time']} for entry in self.tree]
+					   "last_modified_time": entry['last_modified_time']} for entry in items]
 		return self.writeCache(cache_data)
-	
+
 	def writeCache(self, cache_data):
 		"""Writes the generated cache data for the directory to the shelve store.
 
